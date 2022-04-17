59314972393437229b8f4ea0e73d210e59fe654d
rvijaykr10 <rvijaykr10@gmail.com>
2022-04-17 10:56:00 +0530

    added poc text to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,16 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="navItem">
-        <Link to="/">CRUD</Link>
+      <div>
+        <h2>Redux Toolkit POC</h2>
       </div>
-      <div className="navItem">
-        <Link to="/jokes">JOKES</Link>
+      <div className="navItemContainer">
+        <div className="navItem">
+          <Link to="/">CRUD</Link>
+        </div>
+        <div className="navItem">
+          <Link to="/jokes">JOKES</Link>
+        </div>
       </div>
     </div>
   );
